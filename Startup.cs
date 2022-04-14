13d8950fcc9f3286ff483ddcbc65d8983cece69b
David Mimnagh <davidmimnagh1@googlemail.com>
2022-04-14 23:43:53 +0100

    Trying to fix Iodc auth

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace WeatherApp
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<ApplicationUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
